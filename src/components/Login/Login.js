1ab9903bb66d7914c181015aec63f2795544bbd5
unknown <huy.do02062004@gmail.com>
2025-06-04 20:51:20 +0700

    video 9.2 react router

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            login
+
+            <button>test bootstrap</button>
+        </div>
+    )
+}
+export default Login
