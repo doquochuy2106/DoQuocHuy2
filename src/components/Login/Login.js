0ab7a939a9fcc8ffcad2909677ca48d779ab12bc
unknown <huy.do02062004@gmail.com>
2025-06-06 13:10:20 +0700

    video 9.3.3 Form Login Responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,27 +2,32 @@ import "./Login.scss";
 
 const Login = (props) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container ">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7 ">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block ">
                         <div className="brand">
-                            ĐỖ QUỐC HUY
+                            HOI DAN IT
                         </div>
                         <div className="detail">
-                            learning everything
+                            Hoi Dan IT helps you connect and share with the people in your life
                         </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+
+                    <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3 ">
+                        <div className="brand d-sm-none">
+                            HOI DAN IT
+                        </div>
                         <input type="text" className="form-control" placeholder="Email address  phone number" />
                         <input type="password" className="form-control" placeholder="Password" />
                         <button className="btn btn-primary">Login</button>
-                        <span className="text-center">Forgot your pasword</span>
+                        <span className="text-center"><a className="forgot-password" href="#">Forgot your pasword</a></span>
                         <hr />
                         <div className="text-center">
                             <button className="btn btn-success">Create a new account</button>
                         </div>
                     </div>
+
                 </div>
 
             </div>
