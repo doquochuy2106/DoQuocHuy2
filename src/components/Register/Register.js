57378568f4a10dbef82aa7acbcdf71df6f369d1d
unknown <huy.do02062004@gmail.com>
2025-06-12 02:24:32 +0700

    video 10.3.1 Login Form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from 'react-toastify';
+import { registerNewUser } from "../../services/userService";
 
 const Register = (props) => {
     const [email, setEmail] = useState("");
@@ -72,14 +73,20 @@ const Register = (props) => {
         return true
     }
 
-    const handleRegister = () => {
+    const handleRegister = async () => {
 
         let check = isValidInputs();
 
         if (check === true) {
-            axios.post('http://localhost:8081/api/v1/register', {
-                email, phone, username, password
-            })
+            let response = await registerNewUser(email, phone, username, password);
+            let serverData = response.data;
+            if (+serverData.EC == 0) {
+                toast.success(serverData.EM)
+                history.push('/login');
+            }
+            else {
+                toast.error(serverData.EM)
+            }
         }
     }
 
